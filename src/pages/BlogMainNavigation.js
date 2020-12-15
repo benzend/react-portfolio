a4c64628b53331b82d66a46d1323782d932c6c28
Benjamin <1234hteimaj4321@gmail.com>
2020-12-15 14:33:11 -0700

    adding styling

--- src/pages/BlogMainNavigation.js
+++ src/pages/BlogMainNavigation.js
@@ -1,12 +1,28 @@
-import { Typography } from "@material-ui/core";
+import { Typography, makeStyles, Box } from "@material-ui/core";
 
 import { BlogNav } from "./blog-page/components/BlogNav";
 
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  title: {},
+  header: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100px",
+  },
+}));
+
 export const BlogMainNavigation = () => {
+  const { header } = useStyles();
   return (
-    <div>
+    <Box>
       <BlogNav />
-      <Typography variant="h1">Blog</Typography>
-    </div>
+      <Box className={header} component="header">
+        <Typography color="textPrimary" component="h1" variant="h3">
+          MadeUnlinked
+        </Typography>
+      </Box>
+    </Box>
   );
 };
