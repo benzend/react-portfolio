4e57d7dc822b5dd45caaa29abbbdf54414515bca
Benjamin <1234hteimaj4321@gmail.com>
2020-12-16 07:44:58 -0700

    added more content to react intro

--- src/pages/blogs/BlogReactIntro.js
+++ src/pages/blogs/BlogReactIntro.js
@@ -149,14 +149,128 @@ export const BlogReactIntro = () => {
             </code>
             <br />
             <code>
-              <span className="code-block--white">};</span>
+              <span className="code-block--white">{"};"}</span>
             </code>
           </pre>
+          <Typography align="center" color="textPrimary" variant="body1">
+            As you can see from this, a React file tends to contain an import
+            for 'React' in order to use it. However if you initially build an
+            app with `<code>npx create-react-app</code>` then importing it isn't
+            required.
+          </Typography>
+          <Typography
+            align="center"
+            color="textPrimary"
+            variant="h4"
+            component="h2"
+          >
+            ES6
+          </Typography>
+          <Typography align="center" color="textPrimary" variant="body1">
+            If you're unfamiliar with ES6 syntax which is commonly used in React
+            then I would highly encourage you to brush up on your JavaScript as
+            higher order functions like
+            <code>.map</code> and <code>.filter</code> come in handy. Arrow
+            functions are also prevelent and common to see in React code. (They
+            look like this: <code>{"() => {}"}</code>) Oh and you can't forget
+            about object and array destructuring. Now let's go ahead and jump
+            into this code!
+          </Typography>
+          <Typography
+            align="center"
+            color="textPrimary"
+            variant="h4"
+            component="h2"
+          >
+            JSX
+          </Typography>
+          <Typography align="center" color="textPrimary" variant="body1">
+            What is JSX you ask? Well, it's HTML. No, wait, it just{" "}
+            <em>looks</em> like HTML. Since React is a JavaScript library
+            EVERYTHING in React is JavaScript. JSX is just a "syntactic sugar"
+            that covers up the actual ugly code that lays behind it. JSX is
+            this:
+          </Typography>
+          <pre className="code-block">
+            <code>
+              {"  "}
+              <span className="code-block--pink">return </span>
+              <span className="code-block--white">(</span>
+            </code>
+            <br />
+            <code>
+              {"    "}
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">div</span>
+              <span className="code-block--gray">{">"} </span>
+            </code>
+            <br />
+            <code>
+              {"        "}
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">h1</span>
+              <span className="code-block--gray">{">"}</span>
+              <span className="code-block--white">Hello World!</span>
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">/h1</span>
+              <span className="code-block--gray">{">"}</span>
+            </code>
+            <br />
+            <code>
+              {"    "}
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">/div</span>
+              <span className="code-block--gray">{">"} </span>
+            </code>
+          </pre>
+          <Typography align="center" color="textPrimary" variant="body1">
+            One of the best ways to know that you're working with JSX and not
+            HTML is how setting up classes work. In normal, plain old HTML you
+            would use `class` right? Like this:
+          </Typography>
+          <pre className="code-block">
+            <code>
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">h1 </span>
+              <span className="code-block--lightBlue">class</span>
+              <span className="code-block--white">=</span>
+              <span className="code-block--string">"title"</span>
+              <span className="code-block--gray">{">"}</span>
+              <span className="code-block--white">Hello World!</span>
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">/h1</span>
+              <span className="code-block--gray">{">"}</span>
+            </code>
+          </pre>
+          <Typography align="center" color="textPrimary" variant="body1">
+            But guess what? Since everything IS JavaScript, you can't just write
+            `class` because `class` is already defined in JavaScript as a
+            function for making JavaScript classes! So instead we have:
+          </Typography>
+          <pre className="code-block">
+            <code>
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">h1 </span>
+              <span className="code-block--lightBlue">className</span>
+              <span className="code-block--white">=</span>
+              <span className="code-block--string">"title"</span>
+              <span className="code-block--gray">{">"}</span>
+              <span className="code-block--white">Hello World!</span>
+              <span className="code-block--gray">{"<"}</span>
+              <span className="code-block--blue">/h1</span>
+              <span className="code-block--gray">{">"}</span>
+            </code>
+          </pre>
+          <Typography align="center" color="textPrimary" variant="body1">
+            className
+          </Typography>
         </Container>
       </Box>
       <Box className={nav}>
         <Button>An Introduction to React</Button>
         <Button>What is React?</Button>
+        <Button>React Structure</Button>
+        <Button>ES6</Button>
       </Box>
     </Box>
   );
